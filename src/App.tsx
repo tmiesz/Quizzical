8bd7ec86d3d0d92409b2d154882be2ffdb13de1f
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-05-16 13:52:25 +0200

    initial mock setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,37 @@
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="quiz">
+      <div className="quiz-card">
+        <h2>Question 1?</h2>
+        <div className="quiz-answers">
+          <button>Answer 1</button>
+          <button>Answer 2</button>
+          <button>Answer 3</button>
+          <button>Answer 4</button>
+        </div>
+      </div>
+      <div className="quiz-card">
+        <h2>Question 2?</h2>
+        <div className="quiz-answers">
+          <button>Answer 1</button>
+          <button>Answer 2</button>
+          <button>Answer 3</button>
+          <button>Answer 4</button>
+        </div>
+      </div>
+      <div className="quiz-card">
+        <h2>Question 3?</h2>
+        <div className="quiz-answers">
+          <button>Answer 1</button>
+          <button>Answer 2</button>
+          <button>Answer 3</button>
+          <button>Answer 4</button>
+        </div>
+      </div>
+      <div className="quiz-submit">
+        <button>Check answers</button>
+      </div>
+    </div>
+  );
 }
 export default App;
