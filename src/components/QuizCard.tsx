7d3baa44aa46cbc252e2636a403d6f1eddf1a54b
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-05-16 20:30:49 +0200

    html entities

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,3 +1,5 @@
+import { decode } from "html-entities";
+
 type Props = {
   question: string;
   correctAnswer: string;
@@ -10,12 +12,12 @@ export default function QuizCard({
 }: Props) {
   return (
     <div className="quiz-card">
-      <h2>{question}</h2>
+      <h2>{decode(question)}</h2>
       <div className="quiz-answers">
-        <button>{correctAnswer}</button>
-        <button>{incorrectAnswers[0]}</button>
-        <button>{incorrectAnswers[1]}</button>
-        <button>{incorrectAnswers[2]}</button>
+        <button>{decode(correctAnswer)}</button>
+        <button>{decode(incorrectAnswers[0])}</button>
+        <button>{decode(incorrectAnswers[1])}</button>
+        <button>{decode(incorrectAnswers[2])}</button>
       </div>
     </div>
   );
