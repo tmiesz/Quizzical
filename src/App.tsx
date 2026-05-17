45b86c50c7bcf61c3ed791f3d2ebf0882c461dfc
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-05-17 12:17:29 +0200

    refactor and proper answer selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import QuizCard from "./components/QuizCard";
+import QuizCard, { type QuizProps } from "./components/QuizCard";
 import { getQuiz } from "./api";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const { data } = useSuspenseQuery({
@@ -8,17 +10,35 @@ function App() {
     queryFn: () => getQuiz(),
   });
 
-  const quiz = data.results.map((item) => (
-    <QuizCard
-      question={item.question}
-      correctAnswer={item.correct_answer}
-      incorrectAnswers={item.incorrect_answers}
-    />
-  ));
+  const initializeQuiz: QuizProps[] = data.results.map((item) => ({
+    answerIds: Array.from({ length: 4 }, () => uuidv4()),
+    question: item.question,
+    correctAnswer: item.correct_answer,
+    incorrectAnswers: item.incorrect_answers,
+    selected: null,
+    selectAnswer: selectAnswer,
+  }));
+  const [quiz, setQuiz] = useState(initializeQuiz);
+
+  function selectAnswer(id: string) {
+    console.log(id);
+    setQuiz((prev) =>
+      prev.map((q) => (q.answerIds.includes(id) ? { ...q, selected: id } : q)),
+    );
+  }
 
   return (
     <div className="quiz">
-      {quiz}
+      {quiz.map((item) => (
+        <QuizCard
+          answerIds={item.answerIds}
+          question={item.question}
+          correctAnswer={item.correctAnswer}
+          incorrectAnswers={item.incorrectAnswers}
+          selected={item.selected}
+          selectAnswer={item.selectAnswer}
+        />
+      ))}
       <div className="quiz-submit">
         <button>Check answers</button>
       </div>
--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,24 +1,50 @@
 import { decode } from "html-entities";
-import QuizQuestion from "./QuizQuestion";
+import clsx from "clsx";
 
-type Props = {
+export type QuizProps = {
+  answerIds: string[];
   question: string;
   correctAnswer: string;
   incorrectAnswers: string[];
+  selected: string | null;
+  selectAnswer: (id: string) => void;
 };
 export default function QuizCard({
+  answerIds,
   question,
   correctAnswer,
   incorrectAnswers,
-}: Props) {
+  selected,
+  selectAnswer,
+}: QuizProps) {
   return (
     <div className="quiz-card">
       <h2>{decode(question)}</h2>
       <div className="quiz-answers">
-        <QuizQuestion answer={correctAnswer} selected={true} />
-        <QuizQuestion answer={incorrectAnswers[0]} selected={false} />
-        <QuizQuestion answer={incorrectAnswers[1]} selected={false} />
-        <QuizQuestion answer={incorrectAnswers[2]} selected={false} />
+        <button
+          className={clsx(selected === answerIds[0] ? "selected" : "")}
+          onClick={() => selectAnswer(answerIds[0])}
+        >
+          {decode(incorrectAnswers[0])}
+        </button>
+        <button
+          className={clsx(selected === answerIds[1] ? "selected" : "")}
+          onClick={() => selectAnswer(answerIds[1])}
+        >
+          {decode(incorrectAnswers[1])}
+        </button>
+        <button
+          className={clsx(selected === answerIds[2] ? "selected" : "")}
+          onClick={() => selectAnswer(answerIds[2])}
+        >
+          {decode(incorrectAnswers[2])}
+        </button>
+        <button
+          className={clsx(selected === answerIds[3] ? "selected" : "")}
+          onClick={() => selectAnswer(answerIds[3])}
+        >
+          {decode(correctAnswer)}
+        </button>
       </div>
     </div>
   );
