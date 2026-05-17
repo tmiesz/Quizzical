3f882c34dfc0b6d92f83f72c494caf696de25a1d
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-05-17 12:56:42 +0200

    better button generation

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -9,6 +9,7 @@ export type QuizProps = {
   selected: string | null;
   selectAnswer: (id: string) => void;
 };
+
 export default function QuizCard({
   answerIds,
   question,
@@ -17,34 +18,22 @@ export default function QuizCard({
   selected,
   selectAnswer,
 }: QuizProps) {
+  const numberOfAnswers = incorrectAnswers.length + 1;
+
   return (
     <div className="quiz-card">
       <h2>{decode(question)}</h2>
       <div className="quiz-answers">
-        <button
-          className={clsx(selected === answerIds[0] ? "selected" : "")}
-          onClick={() => selectAnswer(answerIds[0])}
-        >
-          {decode(incorrectAnswers[0])}
-        </button>
-        <button
-          className={clsx(selected === answerIds[1] ? "selected" : "")}
-          onClick={() => selectAnswer(answerIds[1])}
-        >
-          {decode(incorrectAnswers[1])}
-        </button>
-        <button
-          className={clsx(selected === answerIds[2] ? "selected" : "")}
-          onClick={() => selectAnswer(answerIds[2])}
-        >
-          {decode(incorrectAnswers[2])}
-        </button>
-        <button
-          className={clsx(selected === answerIds[3] ? "selected" : "")}
-          onClick={() => selectAnswer(answerIds[3])}
-        >
-          {decode(correctAnswer)}
-        </button>
+        {Array.from({ length: numberOfAnswers }, (_, i) => (
+          <button
+            className={clsx(selected === answerIds[i] ? "select" : "")}
+            onClick={() => selectAnswer(answerIds[i])}
+          >
+            {i === numberOfAnswers - 1
+              ? decode(correctAnswer)
+              : decode(incorrectAnswers[i])}
+          </button>
+        ))}
       </div>
     </div>
   );
